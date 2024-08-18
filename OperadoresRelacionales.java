b9f689ba774da58f332a483138f2909c9e8e4e93
Jaime Alberto Suarez Moctezuma <154639506+JaimeMoc@users.noreply.github.com>
2024-08-18 15:56:47 -0600

    Add files via upload

---
+++ OperadoresRelacionales.java
@@ -0,0 +1,17 @@
+package main;
+
+public class OperadoresRelacionales {
+	
+	public static void main(String[] args) {
+	
+		// Operadores relacionales. Teninedo un valor booleano. 
+		// Declaración de las variables.
+		int variable1 = 300, variable2 = 200;
+		boolean resultado;
+		
+		// Realización de la operación matematica.
+		resultado = variable1 > variable2;
+		// Impresión del resultado. 
+		System.out.println(resultado);
+	}
+}
